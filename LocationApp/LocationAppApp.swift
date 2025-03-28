cb30b33b8ff2e63a7a3db5d476a50c1b078e093d
Udhayanila <nilamohan07@outlook.com>
2025-03-28 13:26:22 +0530

    Initial Commit

---
+++ LocationApp/LocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAppApp.swift
+//  LocationApp
+//
+//  Created by Udhayanila on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
